256ed4c1195ba43aadbedcde55fa2238b3efaf9b
bundasse <ymtotolove@gmail.com>
2023-10-22 13:42:49 +0900

    css 마무리, js 코드작성, readme 작성 완료

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,1 +1,8 @@
-// document.getElementById('rating').setAttribute
+document.querySelector('#submit').addEventListener('click', function(){
+    // rate값을 rateResult에 출력
+    let value = document.querySelector('input[name="rate"]:checked').value;
+    document.querySelector('#rateResult').innerHTML = value;
+    // rating에서 show 클래스를 지우고 thankyou에 추가
+    document.querySelector('#rating').classList.remove('show');
+    document.querySelector('#thankyou').classList.add('show');
+});
